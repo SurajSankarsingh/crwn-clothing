be9b34c80e1def80c9ec63cf8966e5ce7582ce7b
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2020-09-29 12:01:59 -0400

    added sign in component and sign in-sign up page with custom button

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,8 +5,8 @@ import SHOP_DATA from "./shop.data.js";
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = {
       collections: SHOP_DATA,
