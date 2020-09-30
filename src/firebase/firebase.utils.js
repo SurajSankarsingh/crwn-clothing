0d201d58b46fccf26919df72e2d7bc125f8a0206
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2020-09-29 23:28:07 -0400

    added user database with firebase and checked logging

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,10 +10,36 @@ const config = {
   storageBucket: "crwn-db-7c1fb.appspot.com",
   messagingSenderId: "868840725330",
   appId: "1:868840725330:web:caad36a08981a88fa2788b",
-  measurementId: "G-48714Z9LLM"
+  measurementId: "G-48714Z9LLM",
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
